f9907ac539af5cb89e9dfee1de1719e8b73ba793
Srikanth1916 <39981109+Srikanth1916@users.noreply.github.com>
2021-11-05 15:27:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-east-2"
-  AWS_ACCESS_KEY_ID = "AKIASJLBDMGRYO22WQVS"
-  AWS_SECRET_ACCESS_KEY = "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"
 }
 
 #Creating the ec2 instance using terraform:
