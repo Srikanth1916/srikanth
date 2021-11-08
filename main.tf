bf1034e1d9bf0839a6881418c963880a58cce5e7
Srikanth1916 <39981109+Srikanth1916@users.noreply.github.com>
2021-11-08 14:18:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 provider "aws" {
-  region = "us-east-2"
 }
 
 #Creating the ec2 instance using terraform:
 
 resource "aws_instance"  "ec2" {
-  ami           = "ami-0f19d220602031aed"
+  ami           = "ami-00dfe2c7ce89a450b"
   instance_type = "t2.micro"
    
 tags = {
